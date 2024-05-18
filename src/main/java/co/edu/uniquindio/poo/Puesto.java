4f221a9965da08d8db301be90d0adf4942c66ded
DeividMateo <deividm.cananp@uqvirtual.edu.co>
2024-05-18 12:45:23 -0500

    puestos

--- src/main/java/co/edu/uniquindio/poo/Puesto.java
+++ src/main/java/co/edu/uniquindio/poo/Puesto.java
@@ -1,7 +1,11 @@
 package co.edu.uniquindio.poo;
 
+import java.util.HashMap;
+
 public class Puesto {
 
     private final Registro registro;
+       var puesto = new HashMap<String, String>();
+       puesto.put("1","Ocupado");
     
 }
