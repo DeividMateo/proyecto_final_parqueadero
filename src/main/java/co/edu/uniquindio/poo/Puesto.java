3392d5d95e686f274f614215977f54794201906a
Deivid Mateo Cañan Pretel <149835310+DeividMateo@users.noreply.github.com>
2024-05-21 16:51:38 -0500

    :)

--- src/main/java/co/edu/uniquindio/poo/Puesto.java
+++ src/main/java/co/edu/uniquindio/poo/Puesto.java
@@ -9,15 +9,15 @@ public class Puesto {
        public static void main(String[] args) {
         Map<String,Puesto>mapaPuestos=new HashMap<>();
         mapaPuestos.put("1,1",new Puesto());
-        mapaPuestos.put("1,2","Moto");
-        mapaPuestos.put("1,3", "Moto");
-        mapaPuestos.put("1,4", "Carro");
-        mapaPuestos.put("1,5", "Carro");
-        mapaPuestos.put("2,1", "Moto");
-        mapaPuestos.put("2,2", "Carro");
-        mapaPuestos.put("2,3", "Moto");
-        mapaPuestos.put("2,4", "Carro");
-        mapaPuestos.put("2,5", "Carro");
+        mapaPuestos.put("1,2",new Puesto());
+        mapaPuestos.put("1,3", new Puesto());
+        mapaPuestos.put("1,4", new Puesto());
+        mapaPuestos.put("1,5", new Puesto());
+        mapaPuestos.put("2,1", new Puesto());
+        mapaPuestos.put("2,2", new Puesto());
+        mapaPuestos.put("2,3", new Puesto());
+        mapaPuestos.put("2,4", new Puesto());
+        mapaPuestos.put("2,5", new Puesto());
         
 
        
