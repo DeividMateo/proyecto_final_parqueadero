fdd341240c3da7f4e66b88bc8106fbfd26529f18
Deivid Mateo Cañan Pretel <149835310+DeividMateo@users.noreply.github.com>
2024-05-21 16:29:20 -0500

    :)

--- src/main/java/co/edu/uniquindio/poo/Puesto.java
+++ src/main/java/co/edu/uniquindio/poo/Puesto.java
@@ -1,12 +1,27 @@
 package co.edu.uniquindio.poo;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Puesto {
 
     private final Registro registro;
-       var puesto = new HashMap<String, String>();
-       puesto.put("1","Ocupado");
+       public static void main(String[] args) {
+        Map<String,Puesto>mapaPuestos=new HashMap<>();
+        mapaPuestos.put("1,1",new Puesto());
+        mapaPuestos.put("1,2","Moto");
+        mapaPuestos.put("1,3", "Moto");
+        mapaPuestos.put("1,4", "Carro");
+        mapaPuestos.put("1,5", "Carro");
+        mapaPuestos.put("2,1", "Moto");
+        mapaPuestos.put("2,2", "Carro");
+        mapaPuestos.put("2,3", "Moto");
+        mapaPuestos.put("2,4", "Carro");
+        mapaPuestos.put("2,5", "Carro");
+        
+
        
+        
+       }
     
 }
